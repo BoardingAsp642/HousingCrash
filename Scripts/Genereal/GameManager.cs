eb83dd03783bbc0fc3dc3a460baff266ab0715c5
BoardingAsp642 <letsmakeagame642@gmail.com>
2023-06-13 14:48:11 -0700

    Setting up Game Manager and UI

--- Scripts/Genereal/GameManager.cs
+++ Scripts/Genereal/GameManager.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables
+    public int health;
+    public int points;
+    private bool gameAcive;
+
+
+    [Header("Components")]
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI pointText;
+    public EndScreenUI endScreen;
+
+    [Header("Events")]
+
+    public UnityEvent onMoneyChanged;
+
+    //Singleton
+    public static GameManager instance;
+
+
+    /*private void Awake()
     {
-        
+        instance = this;
+    }*/
+
+    private void Start()
+    {
+        gameAcive = true;
+        UpdateHealthText();
+        UpdatePointText();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateHealthText()
     {
-        
+        healthText.text = $"Health: {health}";
     }
+
+    void UpdatePointText()
+    {
+        pointText.text = $"Points: {points}";
+    }
+
+
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        UpdateHealthText();
+
+        if (health <= 0)
+        {
+            GameOver();
+            pointText.text = $"Points: {points == 0}";
+        }
+    }
+
+    void GameOver()
+    {
+        gameAcive = false;
+        endScreen.gameObject.SetActive(true);
+    }
+
+    void WinGame()
+    {
+        gameAcive = false;
+        endScreen.gameObject.SetActive(true);
+    }
+
+    /*public void OnEnemyDestroyed()
+    {
+
+        if (!gameAcive)
+            return;
+        if (waverSpawner.remainingEnemies == 0 && waverSpawner.curWave == waverSpawner.waves.Length)
+        {
+            WinGame();
+        }
+
+
+    }*/
 }
