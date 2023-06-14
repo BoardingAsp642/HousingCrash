3c1b665e377d0a35f0a60f402b7369fb097bd3ae
BoardingAsp642 <114502660+BoardingAsp642@users.noreply.github.com>
2023-06-14 02:12:41 -0700

    Setup

--- Scripts/Genereal/GameManager.cs
+++ Scripts/Genereal/GameManager.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables
+    public int points;
+    public int health;
+    private bool gameAcive;
+
+
+    [Header("Components")]
+    public TextMeshProUGUI healthAndPointsText;
+
+    [Header("Events")]
+
+    public UnityEvent onMoneyChanged;
+
+    //Singleton
+    public static GameManager instance;
+
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        gameAcive = true;
+        UpdateHealthAndPointsText();
     }
+
+    void UpdateHealthAndPointsText()
+    {
+        healthAndPointsText.text = $"Health: {health}\nMoney: ${points}";
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        UpdateHealthAndPointsText();
+
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        gameAcive = false;
+
+    }
+
+    void WinGame()
+    {
+        gameAcive = false;
+    }
+
+    /*public void OnEnemyDestroyed()
+    {
+
+        if (!gameAcive)
+            return;
+        if ()
+        {
+            WinGame();
+        }
+
+
+    }*/
 }
